c9f217ecbb00ccc21a8007694bd7279eaa521b0d
سید حسن تقوی <taghavey.hassan@gmail.com>
2025-01-30 22:00:09 +0330

    successfully tested

--- src/Content.js
+++ src/Content.js
@@ -30,15 +30,15 @@ function Content() {
     };
 
     return (
-        <div className='flex flex-col relative w-screen min-h-screen overflow-hidden'>
+        <div className='flex flex-col relative w-screen min-h-screen overflow-hidden scroll-smooth'>
             <div className='flex flex-row justify-center items-center bg-red-200 h-screen w-screen text-3xl text-indigo-800'>Section 1</div>
             <div className='flex flex-row justify-center items-center bg-emerald-200 h-screen w-screen text-3xl text-indigo-800'>Section 2</div>
             <div className='flex flex-row justify-center items-center bg-indigo-200 h-screen w-screen text-3xl text-indigo-800'>Section 3</div>
             <div className='flex flex-row justify-center items-center bg-rose-200 h-screen w-screen text-3xl text-indigo-800'>Section 4</div>
 
             <div className='h-screen w-screen fixed'>
-                <Fade right>
-                    <button href='#' onClick={scrollToTop} className={`absolute rounded-full shadow-md text-white shadow-red-900 duration-300 hover:shadow-none cursor-pointer right-10 bottom-10 ${scroll ? '' : 'hidden'}`}><Icon size={48} icon={arrowUpCircle} /></button>
+                <Fade right className="cursor-pointer">
+                    <button href='#' onClick={scrollToTop} className={`absolute rounded-full shadow-md text-white shadow-red-900 duration-300 hover:shadow-none cursor-pointer right-10 bottom-10 ${scroll ? '' : 'hidden'}`}><Icon size={48} icon={arrowUpCircle} className='cursor-pointer'/></button>
                 </Fade>
             </div>
         </div>
